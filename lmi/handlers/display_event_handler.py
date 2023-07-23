d1bc898d198af1830774aa8f404ff2ccde932c47
Jacob Valdez <jacobfv123@gmail.com>
2023-07-23 14:50:09 -0500

    save

--- lmi/handlers/display_event_handler.py
+++ lmi/handlers/display_event_handler.py
@@ -1,15 +1,30 @@
 import attr
+from lmi.abstract.interactable import LLMCanInteractWithMixin
 
-from gptos.lmi.handlers.event_handler import EventHandler
+from lmi.handlers.event_handler import EventHandler
 
 
-class DisplayEventHandler(EventHandler):
+class DisplayEventHandler(EventHandler, LLMCanInteractWithMixin):
     @attr.s(auto_attribs=True)
     class DisplayEvent(EventHandler.Event):
         pass
 
     def on_show(self, event: DisplayEvent):
+        """Triggered when a component is starts being shown"""
         pass
 
     def on_hide(self, event: DisplayEvent):
+        """Triggered when a component stops being shown"""
         pass
+
+    @property
+    def on_show_tool(self) -> [BaseTool]:
+        return ...
+
+    @property
+    def on_hide_tool(self) -> [BaseTool]:
+        return ...
+
+    @property
+    def tools(self) -> list:
+        return super().tools + [self.on_show_tool, self.on_hide_tool]
